e8b4fb2167bded27a50e24e6711e5fd2e041de34
Austin Liu <2ea64fad26@gmail.com>
2022-08-08 00:22:18 -0500

    created generateGraph benchmarks

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,88 +1,128 @@
-use std::env;
-use graph_coloring::{input::*,ordering::*}; 
-
-pub enum Test{
-    generate_graph(), 
-    generate_random(),
-    vertex_ordering(), 
-    coloring_algorithm(),
-}
+use std::{io::*,env,fs};
+use graph_coloring::{graph::*,input::*,ordering::*}; 
+use std::time::{Duration,Instant}; 
 
 fn main() {
     let args : Vec<String> = env::args().collect(); 
 
-    let test = &args[1]; 
+    if args.len() < 3 {
+        panic!("Wrong number of arguments found."); 
+    }
 
-    match test { 
+    let command = &args[1]; 
+    let directory = &args[2]; 
+
+    match &command[..] { 
         "graph" => {
-            if args.len() < 3{
+            if args.len() < 4{
                 panic!("Wrong number of arguments found."); 
             }
-            let graph = &args[2];
-            match graph {
-                "complete" || "cycle" => {
-                    if args.len() != 3 {
+            let graph = &args[3];
+            match &graph[..] {
+                "complete" => {
+                    if args.len() != 4 {
                         panic!("Wrong number of arguments found."); 
                     }
-                    generate_graph(graph); 
+                    generateGraph(directory, GraphKind::complete); 
                 },
-                "random" => {
-                    if args.len() != 5 {
+                "cycle" => {
+                    if args.len() != 4 {
                         panic!("Wrong number of arguments found."); 
                     }
-                    let dist = &args[3]; 
-                    let density = &args[4]; 
-                    generate_random(graph, dist, density);
+                    generateGraph(directory, GraphKind::cycle); 
                 },
-                _ => panic!("Not a graph type."); 
+                // "random" => {
+                //     if args.len() != 6 {
+                //         panic!("Wrong number of arguments found."); 
+                //     }
+                //     let dist = &args[4]; 
+                //     let density = &args[5]; 
+                //     match dist[..] {
+                //         "uniform" => {
+                //             generateRandom(directory, GraphKind::random, DistKind::uniform, density);
+                //         },
+                //         "skewed" => {
+                //             generateRandom(directory, GraphKind::random, DistKind::skewed, density);
+                //         },
+                //         "cosine" => {
+                //             generateRandom(directory, GraphKind::random, DistKind::cosine, density);
+                //         },
+                //         _ => panic!("Not a distribution type."), 
+                //     }
+                    
+                // },
+                _ => panic!("Not a graph type."),
             }
         },
-        "order" => {
-            if args.len() != 3{
-                panic!("Wrong number of arguments found."); 
-            }
-            let order = &args[2]; 
-            match order { 
-                "SLVO" || "SODL" || "URO" || "BFSR" || "BFSS" || "BFSR" => {
-                    if args.len() != 3 {
-                        panic!("Wrong number of arguments found."); 
-                    }
-                    vertex_ordering(order); 
-                }
-                _ => panic!("Not a vertex ordering."); 
-            }
-        },
-        "color" => {
-            if args.len() < 3{
-                panic!("Wrong number of arguments found."); 
-            }
-            coloring_algorithm 
-            
-        }
-        _ => panic!("Not a valid argument."); 
+        // "order" => {
+        //     if args.len() != 4{
+        //         panic!("Wrong number of arguments found."); 
+        //     }
+        //     let order = &args[3]; 
+        //     match order[..] { 
+        //         "SLVO" | "SODL" | "URO" | "BFSR" | "BFSS" | "BFSR" => {
+        //             if args.len() != 4 {
+        //                 panic!("Wrong number of arguments found."); 
+        //             }
+        //             vertexOrdering(order); 
+        //         }
+        //         _ => panic!("Not a vertex ordering."),
+        //     }
+        // },
+        // "color" => {
+        //     if args.len() < 4{
+        //         panic!("Wrong number of arguments found."); 
+        //     }
+        //     coloringAlgorithm(); 
+        // }
+        _ => panic!("Not a valid argument."), 
     }
 
 }
 
-//runtime complexity for 
-pub fn generate_graph() 
+//runtime complexity for various sizes 
+pub fn generateGraph(dir : &str, graph : GraphKind)
 {
-
+    let sizes : Vec<u32> = vec![10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200];//, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]; 
+    let mut runtime : Vec<u128> = Vec::new(); 
+    fs::create_dir("../tmp/".to_owned() + dir).expect("create failed"); 
+    for v in sizes.iter(){
+        let input : Input = Input{
+            vertices: *v,
+            edges: match graph {
+                GraphKind::complete => (*v)*(*v-1)/2, 
+                GraphKind::cycle => *v, 
+                _ => 0,
+            },
+            graph: graph.clone(),  
+            dist: None, 
+        }; 
+        let start = Instant::now(); 
+        let graph : Graph = Graph::new(input); 
+        let end = start.elapsed(); 
+        println!("{}", end.as_nanos()); 
+        runtime.push(end.as_nanos()); 
+        graph.output(&format!("../tmp/{}/file{}",dir,*v)[..]); 
+    }
+    let mut runtimeFile = std::fs::File::create("../tmp/".to_owned() + dir + "/runtimeFile").expect("create failed");
+    for i in 0..sizes.len() {
+        write!(runtimeFile, "{},{}\n", sizes[i], runtime[i]); 
+    }
 }
 
 //runtime complexity for different sizes of random graphs with various densities 
-pub fn generate_random() 
-{
+// pub fn generateRandom() 
+// {
 
-}
+// }
 
-//
-pub fn vertex_ordering() 
-{
+// //
+// pub fn vertexOrdering() 
+// {
 
-}
+// }
 
-pub fn coloring_algorithm() 
-{
+// pub fn coloringAlgorithm() 
+// {
 
-}
+// }
